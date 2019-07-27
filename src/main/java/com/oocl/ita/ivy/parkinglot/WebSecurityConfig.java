d7fb4c8fc59fea9beda02496c77795d85088e199
Teng YANG <morenyang88@gmail.com>
2019-07-27 21:32:52 +0800

    Disable csrf in spring security

--- src/main/java/com/oocl/ita/ivy/parkinglot/WebSecurityConfig.java
+++ src/main/java/com/oocl/ita/ivy/parkinglot/WebSecurityConfig.java
@@ -14,5 +14,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/**").permitAll();
+        http.csrf().disable();
     }
 }
