cd8f9a20a1bae0ddc213e0eb9c3448eb79a23a60
Teng YANG <morenyang88@gmail.com>
2019-07-27 20:52:17 +0800

    update build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-mv build/libs/parking-lot-0.0.1-SNAPSHOT.jar /approot/parking-lot-server
+chmod 777 build/libs/parking-lot-0.0.1-SNAPSHOT.jar
+mv -f build/libs/parking-lot-0.0.1-SNAPSHOT.jar /approot/parking-lot-server
 nohup java -jar /approot/parking-lot-server/parking-lot-0.0.1-SNAPSHOT.jar > /approot/parking-lot-server/logfile 2>&1 &
 echo "Start successfully"
