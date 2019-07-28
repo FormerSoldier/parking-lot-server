2c420ea7a919d70826e42415fc82555ede302c05
catch77 <1297056296@qq.com>
2019-07-28 12:42:18 +0800

    ADD:  parkingboyService test

--- src/test/java/com/oocl/ita/ivy/parkinglot/service/ParkingBoyServiceTest.java
+++ src/test/java/com/oocl/ita/ivy/parkinglot/service/ParkingBoyServiceTest.java
@@ -9,10 +9,14 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.ArrayList;
 
 import static com.oocl.ita.ivy.parkinglot.entity.enums.Gender.FEMALE;
+
 import static org.junit.Assert.*;
-import static reactor.core.publisher.Mono.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 public class ParkingBoyServiceTest {
@@ -39,5 +43,37 @@ public class ParkingBoyServiceTest {
         return parkingBoy;
     }
 
+    @Test
+    public void should_return_parking_boy_when_save_parking_boy() {
+        ParkingBoy parkingBoy = generateParkingBoy();
+        System.out.println(parkingBoy.getId());
+        when(parkingBoyRepository.save(any())).thenReturn(parkingBoy);
 
+        ParkingBoy savePkb = parkingBoyService.save(parkingBoy);
+
+        assertEquals(parkingBoy.getId(), savePkb.getId());
+        verify(parkingBoyRepository).save(any());
+    }
+
+    @Test
+    public void should_return_all_parking_boy_when_fetch_all() {
+        when(parkingBoyRepository.findAll()).thenReturn(new ArrayList<ParkingBoy>());
+        parkingBoyService.findAllParkingBoys();
+        verify(parkingBoyRepository).findAll();
+    }
+
+    @Test
+    public void should_return_parking_boy_when_fetch_parking_boy_by_id() throws Exception {
+        ParkingBoy parkingBoy = generateParkingBoy();
+        when(parkingBoyRepository.findById(anyString())).thenReturn(java.util.Optional.of(new ParkingBoy()));
+
+        parkingBoyService.find(parkingBoy.getId());
+        verify(parkingBoyRepository).findById(anyString());
+    }
+
+    @Test
+    public void should_return_parking_boy_when_update_parking_boy_by_id() {
+        ParkingBoy parkingBoy = generateParkingBoy();
+        when(parkingBoyRepository.save(any())).thenReturn(parkingBoy);
+    }
 }
