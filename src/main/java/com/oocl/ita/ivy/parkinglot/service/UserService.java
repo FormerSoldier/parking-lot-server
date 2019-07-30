79dac14c6dbf706a4b464043bca8f67d6ec19cb2
ZHANGGA5 <enju648454858>
2019-07-31 00:46:30 +0800

    damn bug

--- src/main/java/com/oocl/ita/ivy/parkinglot/service/UserService.java
+++ src/main/java/com/oocl/ita/ivy/parkinglot/service/UserService.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 
 @Service
-public class gitUserService implements BaseService<User, String>{
+public class UserService implements BaseService<User, String>{
 
     @Autowired
     private JwtOperator jwtOperator;
