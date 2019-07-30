b4b577375cc71243a652716c1968d77d6e4594f0
Teng YANG <teng.yang@oocl.com>
2019-07-30 23:22:53 +0800

    RESET database structure

--- src/main/resources/db/migration/V3__CREATE_PARKING_LOT.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists parking_lot
-(
-	id varchar(255) not null
-		primary key,
-	address varchar(255) null,
-	capacity int not null,
-	name varchar(255) not null
-)    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8;
