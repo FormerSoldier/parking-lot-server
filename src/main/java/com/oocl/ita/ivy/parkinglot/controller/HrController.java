0b04b6b8a4569bb68bf4eedefe2655c09b1fa5ce
catch77 <1297056296@qq.com>
2019-07-30 15:58:12 +0800

    ADD: HR

---
+++ src/main/java/com/oocl/ita/ivy/parkinglot/controller/HrController.java
@@ -0,0 +1,44 @@
+package com.oocl.ita.ivy.parkinglot.controller;
+
+import com.oocl.ita.ivy.parkinglot.entity.User;
+import com.oocl.ita.ivy.parkinglot.repository.HrRepository;
+import com.oocl.ita.ivy.parkinglot.service.HrService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hrs")
+public class HrController {
+
+    @Autowired
+    private HrService hrService;
+
+    @Autowired
+    private HrRepository hrRepository;
+
+    @PostMapping
+    public User save(@RequestBody  User hr) {
+        return hrService.save(hr);
+    }
+
+    @GetMapping(params = {"page"})
+    public Page<User> findAll(@PageableDefault(size = 15) Pageable pageable) {
+        return hrService.findAll(pageable);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Integer id) {
+        hrService.deleteById(id);
+    }
+
+//    @PutMapping
+//    public User update(@RequestBody User hr) {
+//        return hrService.update(hr);
+//    }
+
+}
--- src/main/java/com/oocl/ita/ivy/parkinglot/service/HrService.java
+++ src/main/java/com/oocl/ita/ivy/parkinglot/service/HrService.java
@@ -1,9 +1,50 @@
 package com.oocl.ita.ivy.parkinglot.service;
 
+import com.oocl.ita.ivy.parkinglot.entity.ParkingBoy;
+import com.oocl.ita.ivy.parkinglot.entity.User;
+import com.oocl.ita.ivy.parkinglot.entity.enums.BusinessExceptionType;
+import com.oocl.ita.ivy.parkinglot.entity.enums.Role;
+import com.oocl.ita.ivy.parkinglot.exception.BusinessException;
+import com.oocl.ita.ivy.parkinglot.repository.HrRepository;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HrService {
 
+    @Autowired
+    private HrRepository hrRepository;
+
+    public User save(User hr) {
+        hr.setRoles(Role.HR.getRole());
+        hr.setPassword(DigestUtils.sha256Hex(hr.getPassword()));
+        return hrRepository.save(hr);
+    }
+
+//    public User update(User hr) {
+//        User oldHr = hrRepository.findById(hr.getId()).orElseThrow(() -> new BusinessException(BusinessExceptionType.RECODE_NOT_FOUNT));
+//        hr.set
+//        ParkingBoy oldParkingBoy=parkingBoyRepository.findById(parkingBoy.getId()).orElseThrow(() ->new BusinessException(BusinessExceptionType.RECODE_NOT_FOUNT));
+//        parkingBoy.setUser(oldParkingBoy.getUser());
+//        return parkingBoyRepository.save(parkingBoy);
+//        return hrRepository.save(hr);
+//    }
+
+    public void deleteById(Integer id) {
+        User hr = hrRepository.findById(id).orElseThrow(() -> new BusinessException(BusinessExceptionType.RECODE_NOT_FOUNT));
+        hr.setDeleteFlag(true);
+        hrRepository.save(hr);
+    }
+
+    public User findById(Integer id) {
+        return hrRepository.findById(id).orElseThrow(() -> new BusinessException(BusinessExceptionType.RECODE_NOT_FOUNT));
+    }
+
+    public Page<User> findAll(Pageable pageable) {
+        return hrRepository.findAll(pageable);
+    }
 
 }
